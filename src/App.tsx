e36f1e7565ba31f69444a77637c2ac1cebc5ab65
Austin Raney <araney@cuahsi.org>
2022-06-06 16:44:54 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  materialRenderers,
+  materialCells,
+} from '@jsonforms/material-renderers';
+import schema from "./schema.json"
+import { JsonForms } from '@jsonforms/react';
 
 function App() {
+  const [data, setData] = useState({});
+  const [s, setS] = useState<object>({})
+
+  const [url, setUrl] = useState<string | undefined>(undefined)
+  const ref = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    console.log(url)
+    const f = async () => {
+      if (url === undefined) {
+        return
+      }
+      const res = await fetch(url)
+      const r = await res.json()
+      setS(r)
+    }
+    f()
+  }, [url, setS])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input type="url" name="Enter schema url" id="url" ref={ref} />
+        <input type="button" onClick={() => { setUrl(ref.current?.value); console.log(ref.current?.value) }} />
+
+      </div>
+      <JsonForms
+        schema={s}
+        data={data}
+        renderers={materialRenderers}
+        cells={materialCells}
+      />
+
     </div>
+
   );
 }
 
